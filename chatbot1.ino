599bf89d0d16cbebf49b1cdf878eb78abeca0c6a
jacobigo <133067856+jacobigo@users.noreply.github.com>
2024-07-30 13:52:18 -0700

    main
    main code for half working chatbot

---
+++ chatbot1.ino
@@ -0,0 +1,119 @@
+
+
+
+#include <WiFi.h>
+#include <Arduino.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char *ssid = "ralphie1";
+const char *password = "869Rlsvt44$";
+const char *gemToken = "AIzaSyDJW-e_IAn4XESJQUqYYQe1qZ-UvVxlNMI";
+const char *gemMax = "50";
+String res = "";
+
+void setup() {
+  //basic wifi connection code
+  
+  Serial.begin(115200);
+
+  Serial.print("is it working?");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+
+  while (!Serial)
+    ;
+
+
+  // wait for WiFi connection
+
+  Serial.print("still trying");
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Serial.println("");
+  Serial.println("Ask your Question : ");
+  while (!Serial.available())
+    ;
+  while (Serial.available()) {
+    char add = Serial.read();
+    res = res + add;
+    delay(1);
+  }
+  int len = res.length();
+  res = res.substring(0, (len - 1));
+  res = "\"" + res + "\"";
+  Serial.println("");
+  Serial.print("Asking Your Question : ");
+  Serial.println(res);
+
+  HTTPClient https;
+
+  if (https.begin("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + (String)gemToken)) {  // HTTPS
+
+    https.addHeader("Content-Type", "application/json");
+    String payload = String("{\"contents\": [{\"parts\":[{\"text\":" + res + "}]}],\"generationConfig\": {\"maxOutputTokens\": " + (String)gemMax + "}}");
+
+    //Serial.print("[HTTPS] GET...\n");
+
+    // start connection and send HTTP header
+    int httpCode = https.POST(payload);
+
+    // httpCode will be negative on error
+    // file found at server
+
+    if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+      String payload = https.getString();
+      //Serial.println(payload);
+
+      DynamicJsonDocument doc(1024);
+
+
+      deserializeJson(doc, payload);
+      String Answer = doc["candidates"][0]["content"]["parts"][0]["text"];
+
+
+      
+      // For Filtering our Special Characters, WhiteSpaces and NewLine Characters
+      Answer.trim();
+      String filteredAnswer = "";
+      for (size_t i = 0; i < Answer.length(); i++) {
+        char c = Answer[i];
+        if (isalnum(c) || isspace(c)) {
+          filteredAnswer += c;
+        } else {
+          filteredAnswer += ' ';
+        }
+      }
+      Answer = filteredAnswer;
+
+
+      Serial.println("");
+      Serial.println("Here is your Answer: ");
+      Serial.println("");
+      Serial.println(Answer);
+    } else {
+      Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+    }
+    https.end();
+  } else {
+    Serial.printf("[HTTPS] Unable to connect\n");
+  }
+  res = "";
+
+
+}
